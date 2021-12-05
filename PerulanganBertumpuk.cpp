edc0c85723e13adb544de8d30a1784bcef2276bc
Hanifudin Robbani <90524882+Luseat@users.noreply.github.com>
2021-12-05 17:18:01 +0700

    Create PerulanganBertumpuk.cpp

---
+++ PerulanganBertumpuk.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+	for(int baris = 1; baris <= 4; baris++) 
+{ 
+ for (int kolom = 1; kolom <= 5; kolom++) 
+ { 
+ cout<<kolom<<" "; 
+ } 
+cout<<endl; 
+}
+return 0;
+}
