3649cc6fbd62a7be40c1363c6453ad3766e0dcf4
Hanifudin Robbani <90524882+Luseat@users.noreply.github.com>
2021-12-12 14:35:59 +0700

    Create Martriks2.cpp

---
+++ Martriks2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int M[10][10];
+    int Nbaris,Nkolom;
+    int baris,kolom;
+
+    Nbaris=3;Nkolom=3;
+    cout<<"Mengisi Matrik ukuran " <<Nbaris <<" x "<<Nkolom<<endl;
+
+    for(baris=0;baris<Nbaris;baris++){
+        cout<<"Input data baris ke-"<<baris+1<<endl;
+        for(kolom=0;kolom<Nkolom;kolom++){
+            cout<<"---Input data kolom ke-"<<kolom+1<<" ? ";
+            cin>>M[baris][kolom];
+        }
+        cout<<endl<<endl;
+    }
+
+     for(baris=0;baris<Nbaris;baris++){
+        for(kolom=0;kolom<Nkolom;kolom++){
+            cout<<M[baris][kolom]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    
+    
+
+    return 0;
+}
