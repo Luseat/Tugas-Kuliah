1ef09e2958286799acde5371dd1714be6468499f
Hanifudin Robbani <90524882+Luseat@users.noreply.github.com>
2022-01-02 11:06:31 +0700

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() 
+{ 
+char namaku[20]; 
+cout<<"Inputkan data nama : "; 
+cin.getline(namaku,20); 
+cout<<"Nama anda adalah : "; 
+cout<<namaku; 
+return 0; 
+}
